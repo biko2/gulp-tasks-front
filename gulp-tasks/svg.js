db6f664d0faba756c995884945ee98cf9cfdf0a3
Oier Bravo Urtasun <oierbravo@gmail.com>
2019-04-04 11:26:14 +0200

    drupal live reload

--- gulp-tasks/svg.js
+++ gulp-tasks/svg.js
@@ -7,10 +7,10 @@ module.exports = function (gulp, plugins, options) {
   'use strict';
 
   
-  gulp.task('svg:sprite-page', function() {
+  gulp.task('svg:compile', function() {
     return gulp.src(options.svg.files)
       .pipe(plugins.svgsprite(options.svg))
       .pipe(gulp.dest(options.svg.destination));
   });
-  gulp.task('svg', gulp.series('svg:sprite-page'));
+  gulp.task('svg', gulp.series('svg:compile'));
 };
